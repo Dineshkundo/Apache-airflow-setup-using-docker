7b6aa74c8d96a141e2183dd4a55107aec47a497a
Dinesh Kundo <60452355+Dineshkundo@users.noreply.github.com>
2025-02-24 14:46:29 +0530

    Update airflow_commands.sh

--- Dockerfile/airflow_commands.sh
+++ Dockerfile/airflow_commands.sh
@@ -1,1 +1,14 @@
 
+#!/usr/bin/env bash
+
+# Initiliase the metastore
+airflow db init
+
+# Run the scheduler in background
+airflow scheduler &> /dev/null &
+
+# Create user
+airflow users create -u dinesh -p dinesh -r Admin -e admin@dinesh.com -f Dinesh -l Kundo
+
+# Run the web server in foreground (for docker logs)
+exec airflow webserver
